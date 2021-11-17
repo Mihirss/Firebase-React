d799054f21cff73064374b4356f6296e99027d36
Mihirss <mihirsoniTM@gmail.com>
2021-11-17 15:27:45 +0530

    Final Fire Base

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 import { db, auth } from "./firebase-config";
+
 import {
   collection,
   addDoc,
@@ -9,19 +10,100 @@ import {
   doc,
   deleteDoc,
 } from "firebase/firestore";
+
 import {
   createUserWithEmailAndPassword,
   onAuthStateChanged,
   signInWithEmailAndPassword,
   signOut,
+  signInWithPopup,
+  GoogleAuthProvider,
 } from "@firebase/auth";
-import react from "react";
 
 function App() {
   const [users, setUsers] = useState([]);
+
+  //Taking data to register in FireStore...
   const [newName, setName] = useState();
   const [age, setAge] = useState();
 
+  //Register Email & Password...
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  //Login Email & Password...
+  const [loginEmail, setLoginEmail] = useState();
+  const [logipassword, setLoginPassword] = useState();
+
+  //Store Logged user for save Login info...
+  const [loggedUser, setLoggedUser] = useState({});
+
+  //Update User Id in FireStore...
+  const updateUserID = async (id, age) => {
+    const userDocs = doc(db, "users", id);
+    const newField = { age: age + 1 };
+    await updateDoc(userDocs, newField);
+  };
+
+  //Update User Name in FireStore...
+  const updateUserName = async (id, name) => {
+    const userDocs = doc(db, "users", id);
+    // const newField = { name: newName };
+    await updateDoc(userDocs, { name: name });
+  };
+
+  //Delete User from FireStore...
+  const deleteUser = async (id) => {
+    const userDocs = doc(db, "users", id);
+    await deleteDoc(userDocs);
+  };
+
+  //on Auth Change add State of Auth in LoggedUser for save login history...
+  onAuthStateChanged(auth, (currentUser) => {
+    setLoggedUser(currentUser);
+  });
+
+  //Register New User in FireStore or Add User In FireStore...
+  const Register = async () => {
+    try {
+      const user = await createUserWithEmailAndPassword(auth, email, password);
+      console.log("User in register", user);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  //Login In WIth Email and Password That are Registred In FireStore...
+  const Login = async () => {
+    try {
+      const user = await signInWithEmailAndPassword(
+        auth,
+        loginEmail,
+        logipassword,
+      );
+      console.log("User in register", user);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  //Logout Current user...
+  const Logout = async () => {
+    await signOut(auth);
+  };
+
+  //Login With Google Account...
+  const LoginWithGoogle = async () => {
+    const Provider = new GoogleAuthProvider();
+    try {
+      const user = await signInWithPopup(auth, Provider);
+      console.log("User in register", user);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  //Add data in FireBase...
   const addData = async () => {
     try {
       const docRef = await addDoc(collection(db, "users"), {
@@ -34,23 +116,7 @@ function App() {
     }
   };
 
-  const updateUser = async (id, age) => {
-    const userDocs = doc(db, "users", id);
-    const newField = { age: age + 1 };
-    await updateDoc(userDocs, newField);
-  };
-
-  const updateUserName = async (id, name) => {
-    const userDocs = doc(db, "users", id);
-    // const newField = { name: newName };
-    await updateDoc(userDocs, { name: name });
-  };
-
-  const deleteUser = async (id) => {
-    const userDocs = doc(db, "users", id);
-    await deleteDoc(userDocs);
-  };
-
+  //Get data from FireBase...
   useEffect(() => {
     const getuser = async () => {
       try {
@@ -64,43 +130,12 @@ function App() {
     getuser();
   }, []);
 
-  const [email, setEmail] = useState();
-  const [loginEmail, setLoginEmail] = useState();
-
-  const [password, setPassword] = useState();
-  const [logipassword, setLoginPassword] = useState();
-
-  const [loggedUser, setLoggedUser] = useState({});
-
-  onAuthStateChanged(auth, (currentUser) => {
-    setLoggedUser(currentUser);
-  });
-
-  const Register = async () => {
-    try {
-      const user = createUserWithEmailAndPassword(auth, email, password);
-      console.log("User in register", user);
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
-  const Login = async () => {
-    try {
-      const user = signInWithEmailAndPassword(auth, loginEmail, logipassword);
-      console.log("User in register", user);
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
-
-  const Logout = async () => {
-    await signOut(auth);
-  };
   return (
-    <>
+    <div className="grid" key="main">
       <div className="App" key="app">
-        <h1>Register user</h1>
-        <div>
+        {/* Register Email... */}
+        <div className="card" key="Register">
+          <h1>Register user</h1>
           <input
             type="email"
             placeholder="Email..."
@@ -117,8 +152,10 @@ function App() {
           />
           <button onClick={Register}>Register user</button>
         </div>
-        <h1>Login user</h1>
-        <div>
+
+        {/* Login With Google & Email... */}
+        <div className="card" key="login">
+          <h1>Login user</h1>
           <input
             type="email"
             placeholder="Email..."
@@ -134,15 +171,20 @@ function App() {
             }}
           />
           <button onClick={Login}>Login user</button>
+          <button onClick={LoginWithGoogle}>Login with GOOGLE</button>
         </div>
-        <div>
+
+        {/* Display Looged in User Email... */}
+        <div className="card" key="Loggedin">
           <h1>Loged in user</h1>
           {loggedUser?.email}
           <button onClick={Logout}>SignOut</button>
         </div>
       </div>
 
-      <div className="App" key="app1">
+      {/* Display All Users Form FireStore... */}
+      <div className="card" key="app1">
+        {/* Register New User... */}
         <h1>List user</h1>
         <input
           type="text"
@@ -159,18 +201,24 @@ function App() {
           }}
         />
         <button onClick={addData}>add to fireStore</button>
+
+        {/* Map each Users from FireStore to */}
         {users.map((user) => {
           return (
-            <div key={user.id}>
+            <div key={user.id} className="card">
               <h1>name:{user.name}</h1>
               <h1>born:{user.age}</h1>
+
+              {/* Update Age by adding 1... */}
               <button
                 onClick={() => {
-                  updateUser(user.id, user.age);
+                  updateUserID(user.id, user.age);
                 }}
               >
                 Upadate Age
               </button>
+
+              {/* Delete User from FireStore... */}
               <button
                 onClick={() => {
                   deleteUser(user.id);
@@ -178,6 +226,8 @@ function App() {
               >
                 Delete User
               </button>
+
+              {/* Update Name by Adding new Name to TextBox */}
               <input
                 type="text"
                 placeholder="UpdateName"
@@ -189,21 +239,8 @@ function App() {
           );
         })}
       </div>
-    </>
+    </div>
   );
 }
 
 export default App;
-
-// import { collection, addDoc } from "firebase/firestore";
-
-// try {
-//   const docRef = await addDoc(collection(db, "users"), {
-//     first: "Ada",
-//     last: "Lovelace",
-//     born: 1815,
-//   });
-//   console.log("Document written with ID: ", docRef.id);
-// } catch (e) {
-//   console.error("Error adding document: ", e);
-// }
